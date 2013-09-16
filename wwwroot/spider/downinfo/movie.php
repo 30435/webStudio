a452d7b5ba27c77f9266d6577f3fa7eb5aa120c3
wangcan <iamwangcan@163.com>
2013-09-17 00:06:11 +0800

    develop the movie app

---
+++ wwwroot/spider/downinfo/movie.php
@@ -0,0 +1,13 @@
+<?php 
+return array(
+	'files' => array(
+		'd:/web/www/webStudio/wwwroot/static/movie/css/base.min.css',
+		'd:/web/www/webStudio/wwwroot/static/movie/css/common.css',
+		'd:/web/www/webStudio/wwwroot/static/movie/css/index.css',
+	),
+
+	'urlpre' => 'http://image1.jmw.com.cn/login/',
+	'localpre' => 'd:/web/www/webStudio/wwwroot/static/movie/images/',
+	'localurl' => 'http://static.ci.com/movie/images/',
+);
+
