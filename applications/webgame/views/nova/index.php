37087d4fa1d4a4b851441ee17c2db3084888e560
iamwangcan <iamwangcan@163.com>
2013-11-01 17:58:53 +0800

    update the front resource

--- applications/webgame/views/nova/index.php
+++ applications/webgame/views/nova/index.php
@@ -8,23 +8,34 @@ function searchFunc()
 </script>
 <!--------------------------宽度一样的977px---------------------------------------->
 <div class="all">
-	<div class="ban"><iframe id="frm" src="<?php echo $this->currentWebgameInfo['url_webgame']; ?>" width="960" height="620" frameborder="0" scrolling="no"></iframe></div>
+	<div class="ban"><iframe class="frm"  src="<?php echo $this->currentWebgameInfo['url_webgame']; ?>" width="956" height="646" frameborder="0" scrolling="no"></iframe></div>
 		<div class="nav_nuowa">
 			<div class="nuowa_bj0">
 				<p class="more"><a href="<?php echo $this->categoryInfos[16]['url']; ?>" target="_blank">更多</a></p>
 				<ul>
 					<?php $newInfos = $controller->_getFrontInfos('webgame', 'new', 1, 6, array('catid' => 10), array(array('inputtime', 'desc'))); ?>
 					<?php foreach ($newInfos['infos'] as $newInfo) { ?>
-						<li><a href="<?php echo $newInfo['thumb']; ?>" target="_blank"><img src="<?php echo $newInfo['thumb']; ?>" width="113px" height="114px" /></a><?php echo $controller->cutstr($newInfo['title'], 32); ?></li>
+						<li><a href="#"><img src="images/001.png" /></a></li>
+						<li><a href="<?php echo $newInfo['thumb']; ?>" target="_blank"><img src="<?php echo $newInfo['thumb']; ?>" width="115px" height="120px" /></a></li>
 					<?php } ?>
 				</ul>
 			</div>
 		</div>
+	</div>
 
-		<div class="footer">
-			<div class="huang"><img src="<?php echo $this->staticUrl; ?>cfront/images/h.png" /></div>
-			<p>小游戏作品版权归原作者享有，如无意之中侵犯了您的版权，请您按照《版权保护投诉指引》来信告知，本网站将应您的要求删除。<br />知金温馨提示：抵制不良游戏 拒绝盗版游戏 注意自我保护 谨防受骗上当 适度游戏益脑 沉迷游戏伤身 合理安排时间 享受健康生活<br/>京ICP备09081115号-6 京网文[2013]0506-133号 网络文化经营许可证<br />知金伟业教育科技（北京）有限公司 Copyright ? 2008 - 2012 ZhiJin Inc. All Rights Reserved.</p>
-		</div>   
+	<div class="footer">
+		<div class="foot_con">
+			<div class="foot_logo fl"><a href="<?php echo $this->currentWebgameInfo['url_home']; ?>"><img src="<?php echo $this->staticUrl; ?>cfront/images/logo.png" /></a></div>
+			<div class="foot_tent fl">
+				<p class="nav_foot">
+				<?php foreach (array(2, 3, 4, 19, 20, 21) as $showCatid) { ?>
+					<a href="<?php echo $this->categoryInfos[$showCatid]['url']; ?>" title="<?php echo $this->categoryInfos[$showCatid]['catname']; ?>"><?php echo $this->categoryInfos[$showCatid]['catname']; ?></a> |
+				<?php } ?>	
+				
+				</p>
+				<p>京ICP备09081115号-6 京网文[2013]0506-133号 网络文化经营许可证<br />知金伟业教育科技（北京）有限公司 Copyright ? 2008 - 2012 ZhiJin Inc. All Rights Reserved.</p>
+			</div>
+		</div>
 	</div>
 </div>
 </body>
