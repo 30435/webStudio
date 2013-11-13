c09cf94792923cc5d2a9ac6376913ed4d68230e3
iamwangcan <iamwangcan@163.com>
2013-11-13 18:27:15 +0800

    develop the juvenile

--- applications/juvenile/views/index.php
+++ applications/juvenile/views/index.php
@@ -1,8 +1,8 @@
 <?php echo $this->load->view('header_base'); ?>
-<link href="<?php echo $this->staticUrl; ?>juvenile/css/common.css" type="text/css" rel="stylesheet"/>
-<link href="<?php echo $this->staticUrl; ?>juvenile/css/global.css" type="text/css" rel="stylesheet" />
-<link href="<?php echo $this->staticUrl; ?>juvenile/css/index.css" type="text/css" rel="stylesheet" />
-<link href="<?php echo $this->staticUrl; ?>juvenile/css/floatAd.css" type="text/css" rel="stylesheet" />
+<link href="<?php echo $this->staticUrl; ?>juvenile/common/css/common.css" type="text/css" rel="stylesheet"/>
+<link href="<?php echo $this->staticUrl; ?>juvenile/common/css/global.css" type="text/css" rel="stylesheet" />
+<link href="<?php echo $this->staticUrl; ?>juvenile/common/css/index.css" type="text/css" rel="stylesheet" />
+<link href="<?php echo $this->staticUrl; ?>juvenile/common/css/floatAd.css" type="text/css" rel="stylesheet" />
 
 <link type="text/css" href="<?php echo $this->staticUrl; ?>juvenile/css/lrtk.css" rel="stylesheet" />
 
@@ -51,29 +51,67 @@ function slideAd(id,nStayTime,sState,nMaxHth,nMinHth){
  slideAd('www_zzjs_net',2);
 -->
 </script>
+<script type="text/javascript" id="bdshare_js" data="type=slide&amp;img=1&amp;pos=left&amp;uid=6519315" ></script>
+
 <div class="header clearfix">
-    <div class="ll block"><a href="http://www.ccppg.com.cn/" target="_blank"><img src="http://www.ccppg.com.cn/images/common/top_logo.gif" width="95" height="28" border="0"/></a></div>
-    <div class="headerLogin ll"><script src="http://www.ccppg.com.cn/e/member/iframe/state.php?name=index"></script></div>
-    <div class="headerSearch ll"><script src="http://www.ccppg.com.cn/d/js/js/search_news1.js"></script></div>
-    <div class="headerOur rr"><a href="http://www.ccppg.com.cn/download/class_3/2013-08-02/231.html" target="_blank"><b><font style=" font-family:Verdana, Geneva, sans-serif; color: #C00">English</font></b></a>&nbsp;&nbsp;<a href="http://www.ccppg.com.cn/baokan/qikan/" onMouseOver="$('.headerOur ul').show('slow');">我们的书报刊</a>
-      <ul class="clearfix" style="display:none;">
-            <li><a href="http://www.ccppg.com.cn/baokan/zhongguoshaonianbao" target="_blank">中国少年报</a></li>
-            <li><a href="http://www.ccppg.com.cn/baokan/zhongguoertongbao" target="_blank">中国儿童报</a></li>
-            <li><a href="http://www.ccppg.com.cn/baokan/zhongguozhongxueshengbao" target="_blank">中国中学生报</a></li>
-            <li><a href="http://www.hongdaishu.com/ethb/index.aspx" target="_blank">中国儿童画报</a></li>
-            <li><a href="http://www.ccppg.com.cn/baokan/zhongguoshaonianyingyubao" target="_blank">中国少年英语报</a></li>
-            <li><a href="http://www.ccppg.com.cn/baokan/zhongxuesheng" target="_blank">中学生</a></li>
-            <li><a href="http://www.ccppg.com.cn/baokan/zhixinjiejie" target="_blank">知心姐姐</a></li>
-            <li><a href="http://www.ccppg.com.cn/baokan/zhongguoshaonianwenzhai" target="_blank">中国少年文摘</a></li>
-            <li><a href="http://www.ccppg.com.cn/baokan/zhongguoshaonianertong" target="_blank">中国少年儿童</a></li>
-            <li><a href="http://www.hongdaishu.com/yehb/Index.aspx" target="_blank">婴儿画报</a></li>
-            <li><a href="http://www.ccppg.com.cn/baokan/zhongguokatong" target="_blank">中国卡通</a></li>
-            <li><a href="http://www.ccppg.com.cn/baokan/etwx" target="_blank">儿童文学</a></li>
-            <li><a href="http://www.ccppg.com.cn/baokan/womenaikexue" target="_blank">我们爱科学</a></li>
-            <li><a href="http://www.youerhuabao.com/" target="_blank">幼儿画报</a></li>
-            <li><a href="http://www.hongdaishu.com/ddx/Index.aspx" target="_blank">嘟嘟熊</a></li>
-            <li><a href="http://www.zhiliketang.com" target="_blank">智力课堂</a></li>
-            <li><a href="javascript:void(0);" class="red" onClick="$('.headerOur ul').hide('slow');">关闭</a></li>
+    <div class="ll block"><a href="<?php echo $this->baseUrl; ?>" target="_blank"><img src="<?php echo $this->staticUrl; ?>juvenile/common/images/common/top_logo.gif" width="95" height="28" border="0"/></a></div>
+
+	<div class="headerLogin ll" style="display:none;">
+		&nbsp;&nbsp;欢迎您，zhijinuser。
+		&nbsp;&nbsp;<a target="_blank" class="blue" href="http://bbs.ccppg.com.cn/home.php?mod=space&amp;do=pm">短消息</a>
+		&nbsp;&nbsp;<a target="_blank" class="blue" href="http://bbs.ccppg.com.cn/home.php?mod=space&amp;do=favorite">收藏夹</a>
+		&nbsp;&nbsp;<a target="_blank" class="blue" href="http://bbs.ccppg.com.cn/home.php">会员中心</a>
+		&nbsp;&nbsp;<a onclick="return confirm('确认要退出吗?');" target="_parent" href="http://www.ccppg.com.cn/e/enews?enews=exit&amp;prtype=9&amp;gotourl=http%3A%2F%2Fwww.ccppg.com.cn%2F">退出</a>
+	</div>
+
+    <div class="headerLogin ll">
+	<form action="http://www.ccppg.com.cn/e/enews/index.php" method="post" name="login">
+		<input type="hidden" value="http://www.ccppg.com.cn/" name="location">
+		<input type="hidden" value="login" name="enews">
+		<input type="hidden" value="1" name="prtype">
+		&nbsp;&nbsp;用户名:&nbsp;<input type="text" style="width:80px" class="ipt_text_1" id="username" name="username">
+		&nbsp;&nbsp;密码:&nbsp;<input type="password" style="width:80px" class="ipt_text_1" id="password" name="password">
+		&nbsp;<input type="submit" class="ipt_btn_1" value="登录" id="submit" name="loginsubmit">
+		<input type="hidden" value="login" name="action">
+		&nbsp;<a target="_parent" class="red" href="http://bbs.ccppg.com.cn/member.php?mod=register.php">注册</a>
+		&nbsp;<a target="_parent" href="http://bbs.ccppg.com.cn/member.php?mod=logging&amp;action=login&amp;viewlostpw">忘记密码</a>
+	</form>
+	</div>
+
+    <div class="headerSearch ll">
+	<form onsubmit="return search_check(document.search_js1);" action="http://www.ccppg.com.cn/e/sch/index.php" method="GET" name="search_js1">
+		<input type="text" title="请输入关键字" value="请输入关键字" style="width:150px; color:#CCCCCC" class="ipt_text_1" id="keyboard" name="keyboard">
+		<input type="hidden" value="1" id="dotime" name="dotime">
+		<select class="sel_1" id="field" name="field">
+			<option value="1">不限</option>
+			<option value="2">标题</option>    
+			<option value="3">内容</option>
+		</select> 
+		<input type="submit" class="ipt_btn_1" value="搜索" id="submit" name="submit">
+	</form>
+	</div>
+
+    <div class="headerOur rr">
+		<a target="_blank" href="http://www.ccppg.com.cn/download/class_3/2013-08-02/231.html"><b><font style=" font-family:Verdana, Geneva, sans-serif; color: #C00">English</font></b></a>&nbsp;&nbsp;
+		<a onmouseover="$('.headerOur ul').show('slow');" href="http://www.ccppg.com.cn/baokan/qikan/">我们的书报刊</a>
+		<ul style="display:none;" class="clearfix">
+            <li><a target="_blank" href="http://www.ccppg.com.cn/baokan/zhongguoshaonianbao">中国少年报</a></li>
+            <li><a target="_blank" href="http://www.ccppg.com.cn/baokan/zhongguoertongbao">中国儿童报</a></li>
+            <li><a target="_blank" href="http://www.ccppg.com.cn/baokan/zhongguozhongxueshengbao">中国中学生报</a></li>
+            <li><a target="_blank" href="http://www.hongdaishu.com/ethb/index.aspx">中国儿童画报</a></li>
+            <li><a target="_blank" href="http://www.ccppg.com.cn/baokan/zhongguoshaonianyingyubao">中国少年英语报</a></li>
+            <li><a target="_blank" href="http://www.ccppg.com.cn/baokan/zhongxuesheng">中学生</a></li>
+            <li><a target="_blank" href="http://www.ccppg.com.cn/baokan/zhixinjiejie">知心姐姐</a></li>
+            <li><a target="_blank" href="http://www.ccppg.com.cn/baokan/zhongguoshaonianwenzhai">中国少年文摘</a></li>
+            <li><a target="_blank" href="http://www.ccppg.com.cn/baokan/zhongguoshaonianertong">中国少年儿童</a></li>
+            <li><a target="_blank" href="http://www.hongdaishu.com/yehb/Index.aspx">婴儿画报</a></li>
+            <li><a target="_blank" href="http://www.ccppg.com.cn/baokan/zhongguokatong">中国卡通</a></li>
+            <li><a target="_blank" href="http://www.ccppg.com.cn/baokan/etwx">儿童文学</a></li>
+            <li><a target="_blank" href="http://www.ccppg.com.cn/baokan/womenaikexue">我们爱科学</a></li>
+            <li><a target="_blank" href="http://www.youerhuabao.com/">幼儿画报</a></li>
+            <li><a target="_blank" href="http://www.hongdaishu.com/ddx/Index.aspx">嘟嘟熊</a></li>
+            <li><a target="_blank" href="http://www.zhiliketang.com">智力课堂</a></li>
+            <li><a onclick="$('.headerOur ul').hide('slow');" class="red" href="javascript:void(0);">关闭</a></li>
         </ul>
     </div>
 </div>
@@ -91,7 +129,7 @@ function slideAd(id,nStayTime,sState,nMaxHth,nMinHth){
             <param name="movie" value="/d/images/index_new/nav_logo.swf" />
             <param name="quality" value="high" />
             <param name="wmode" value="Opaque" />
-            <embed src="/d/images/index_new/nav_logo.swf" quality="high" wmode="Opaque" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="90" height="135"></embed>
+            <embed src="http://www.ccppg.com.cn//d/images/index_new/nav_logo.swf" quality="high" wmode="Opaque" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="90" height="135"></embed>
         </object>
     </div>
     <ul class="ll clearfix">
@@ -102,9 +140,9 @@ function slideAd(id,nStayTime,sState,nMaxHth,nMinHth){
         <li><a href="http://bbs.ccppg.com.cn/home.php?action/spaces""><img src="http://www.ccppg.com.cn/images/common/blog.png" align="absmiddle" name="blog" /></a></li>
     </ul>
 </div>
-<div class="ww mtn">
-  <div class="topRed block"><a href="http://www.ccppg.com.cn/"><img src="http://www.ccppg.com.cn/images/common/ccppg_com_cn.gif" /></a></div>
-</div>
+
+<div class="ww mtn"><div class="topRed block"><a href="http://www.ccppg.com.cn/"><img src="http://www.ccppg.com.cn/images/common/ccppg_com_cn.gif" /></a></div></div>
+
 <div class="ww">
   <script type="text/javascript" src="http://www.ccppg.com.cn/d/js/acmsd/thea97.js"></script>
 </div>
@@ -452,7 +490,7 @@ MarqueeDiv4Control.UnBound = function()
 <script type="text/javascript" src="http://count.ccppg.cn//count/count.js" ></script>
 <!-- /phpstat.net -->
 <!-- Baidu Button BEGIN -->
-<script type="text/javascript" id="bdshare_js" data="type=slide&amp;img=1&amp;pos=left&amp;uid=6519315" ></script>
+
 <script type="text/javascript" id="bdshell_js"></script>
 <script type="text/javascript">
 document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + Math.ceil(new Date()/3600000);
